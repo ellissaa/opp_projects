19076d334e727701e5da4d18f9700be7c5490f78
Brilkova Elizaveta <brilkovaelizaveta@gmail.com>
2024-05-06 22:58:59 +0700

    MPI lab 3

---
+++ lab3/functions.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTIONS3
+#define FUNCTIONS3
+
+int ValidParameters(int A_num_rows, int grid_num_rows, int B_num_cols, int grid_num_cols, int size);
+
+void FillRandom(double* arr, int size);
+
+void PrintMatrix(double* arr, int rows, int cols);
+
+#endif
